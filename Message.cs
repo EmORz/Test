5456d723648fbb08bcc5948645f7f980b8f37d00
Andriana <rosiani@mail.bg>
2019-02-25 17:20:19 +0200

    Add files via upload

---
+++ Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMapper
+{
+    public class Message
+    {
+        public Message()
+        {
+                
+        }
+        public string Name { get; set; }
+    }
+}
