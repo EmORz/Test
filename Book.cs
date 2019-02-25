92e190b1fa6bd1c0de225033903910c0864519e7
Andriana <rosiani@mail.bg>
2019-02-25 17:19:33 +0200

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMapper
+{
+    public class Book
+    {
+        public Book()
+        {
+
+        }
+        public string Content { get; set; }
+
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+
+        public Message Message { get; set; }
+
+        public string[] Authors { get; set; }
+
+       public Message[] Messages { get; set; }
+    }
+}
